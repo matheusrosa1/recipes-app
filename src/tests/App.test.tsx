db3dda13d8c0f0b3ef94b3f590af3ad2d2ee2e05
Mateus Felipe <mateus.mfr10@gmail.com>
2023-11-25 11:24:59 -0300

    feat: testes dos requisitos 2 e 3

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+const emailInputTestId = 'email-input';
+const passwordInputTestId = 'password-input';
+const submitTestId = 'submit-btn';
+
+describe('Testes do requisito 2', () => {
+  it('Verifica se estão corretos os data-testids email-input, password-input e login-submit-btn', () => {
+    render(<App />);
+    const emailInput = screen.getByTestId(emailInputTestId);
+    const passwordInput = screen.getByTestId(passwordInputTestId);
+    const submitButton = screen.getByTestId(submitTestId);
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+});
+
+describe('Testes do requisito 3', () => {
+  it('Verifica se a pessoa consegue escrever seu e-mail no input de email e sua senha no input de senha', () => {
+    render(<App />);
+    const emailTest = 'test@email.com';
+    const passwordTest = '123456789';
+    fireEvent.change(screen.getByTestId(emailInputTestId), { target: { value: emailTest } });
+    fireEvent.change(screen.getByTestId(passwordInputTestId), { target: { value: passwordTest } });
+    const emailInput = screen.getByTestId(emailInputTestId);
+    const passwordInput = screen.getByTestId(passwordInputTestId);
+    expect(emailInput).toHaveValue(emailTest);
+    expect(passwordInput).toHaveValue(passwordTest);
+  });
 });

363f429ea041df4409c66c435c48739fd683de87
marcelaadriany <marcela.adriany@gmail.com>
2023-11-25 12:23:37 -0300

    feat: requisitos 4, 5 e 6

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect, FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  useEffect(() => {
+    const emailPattern = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+    const isEmailValid = emailPattern.test(email);
+    const isPasswordValid = password.length > 6;
+
+    if (isEmailValid && isPasswordValid) {
+      setIsButtonDisabled(false);
+    } else {
+      setIsButtonDisabled(true);
+    }
+  }, [email, password]);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    // salva email no localStorage na chave 'user' no formato { email: email-da-pessoa }
+    localStorage.setItem('user', JSON.stringify({ email }));
+    // redirecionar para a rota /meals
+    navigate('/meals');
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={ handleSubmit }>
         <input
           type="text"
           data-testid="email-input"
+          value={ email }
+          onChange={ handleEmailChange }
         />
         <input
-          type="text"
+          type="password"
           data-testid="password-input"
+          value={ password }
+          onChange={ handlePasswordChange }
         />
-        <button data-testid="login-submit-btn">Enter</button>
+        <button data-testid="login-submit-btn" disabled={ isButtonDisabled }>
+          Enter
+        </button>
       </form>
     </div>
   );
--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from '../App';
 
-test('Farewell, front-end', () => {
-  // Este arquivo pode ser modificado ou deletado sem problemas
-  render(<App />);
-  const linkElement = screen.getByText(/TRYBE/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testes do requisito 2', () => {
+  it('Verifica se estão corretos os data-testids email-input, password-input e login-submit-btn', () => {
+    render(<App />);
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId('password-input');
+    const submitButton = screen.getByTestId('submit-btn');
+    expect(emailInput).toBeInTheDocument();
+    expect(passwordInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
 });

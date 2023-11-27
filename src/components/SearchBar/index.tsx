20978c5987a11cca4a9254fae25f0d0ae98ec781
Mateus Felipe <mateus.mfr10@gmail.com>
2023-11-27 11:01:58 -0300

    feat: requisito 10

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,6 +1,42 @@
 function SearchBar() {
   return (
-    <input type="text" data-testid="search-input" />
+    <form>
+      <input type="text" data-testid="search-input" />
+      <input
+        data-testid="ingredient-search-radio"
+        type="radio"
+        id="ingredientSearchOption"
+        name="searchOption"
+        value="ingredientSearch"
+      />
+      <label htmlFor="ingredientSearchOption">Ingredient</label>
+
+      <input
+        data-testid="name-search-radio"
+        type="radio"
+        id="nameSearchOption"
+        name="searchOption"
+        value="nameSearch"
+      />
+      <label htmlFor="nameSearchOption">Name</label>
+
+      <input
+        data-testid="first-letter-search-radio"
+        type="radio"
+        id="firstLetterSearchOption"
+        name="searchOption"
+        value="firstLetterSearch"
+      />
+      <label htmlFor="firstLetterSearchOption">First letter</label>
+
+      <button
+        type="submit"
+        data-testid="exec-search-btn"
+      >
+        Search
+
+      </button>
+    </form>
   );
 }
 export default SearchBar;

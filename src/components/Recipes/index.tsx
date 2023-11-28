5ae395b1efe1568609276416291db52e6d9a5181
Matheus Chiarello <chiarellomatheus@gmail.com>
2023-11-27 21:07:13 -0300

    requisitos 18 e 19 - ok mas falta testes

--- src/components/Recipes/index.tsx
+++ src/components/Recipes/index.tsx
@@ -1,49 +1,75 @@
 import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import { getDrinksAPI, getMealsAPI } from '../../services/fetchAPI';
+import { getCategoties, getDrinksAPI, getMealsAPI } from '../../services/fetchAPI';
+import { CategoriesType, RecipeType } from '../types';
 
 function Recipes() {
   const location = useLocation();
-  const [recipes, setRecipes] = useState([]);
+  const [recipes, setRecipes] = useState<RecipeType[]>([]);
+  const [categories, setCategories] = useState<CategoriesType[]>([]);
 
+  const getData = async () => {
+    try {
+      const recipesData = location.pathname === '/meals'
+        ? await getMealsAPI() : await getDrinksAPI();
+      const recipesDataSplice = [...recipesData].splice(0, 12);
+      setRecipes(recipesDataSplice as RecipeType[]);
+    } catch (error) {
+      console.log('Erro ao buscar os dados na API', error);
+    }
+  };
+
+  const getCategories = async () => {
+    try {
+      const categoriesData = await getCategoties(location.pathname);
+      const categoriesDataSplice = [...categoriesData].splice(0, 5);
+      setCategories(categoriesDataSplice);
+    } catch (error) {
+      console.log('Erro ao buscar os dados na API', error);
+    }
+  }
+  
   useEffect(() => {
-    const getData = async () => {
-      try {
-        const recipesData = location.pathname === '/meals'
-          ? await getMealsAPI() : await getDrinksAPI();
-        const recipesDataSplice = recipesData.splice(0, 12);
-        setRecipes(recipesDataSplice);
-      } catch (error) {
-        console.log('Erro ao buscar os dados na API', error);
-      }
-    };
     getData();
-  }, []);
+    getCategories();
+  }, []);  
 
   const id = location.pathname === '/meals' ? 'idMeal' : 'idDrink';
   const img = location.pathname === '/meals' ? 'strMealThumb' : 'strDrinkThumb';
   const name = location.pathname === '/meals' ? 'strMeal' : 'strDrink';
 
   return (
+    <>
     <div>
-      {recipes && recipes.map((meal, index) => (
+      {categories && categories.map(({strCategory}: CategoriesType, index) => (
+        <button
+          key={ index }
+          data-testid={ `${strCategory}-category-filter` }
+        >
+          {strCategory}
+        </button>
+      ))}
+    </div>
+    <div>
+      {recipes && recipes.map((recipe: RecipeType, index) => (
         
         <div
-          key={ meal[id] }
+          key={ recipe[id] }
           data-testid={ `${index}-recipe-card` }
         >
           <img
-            src={ meal[img] }
-            alt={ meal[name] }
+            src={ recipe[img] }
+            alt={ recipe[name] }
             data-testid={ `${index}-card-img` }
             width={'100px'}
           />
           <p
             data-testid={ `${index}-card-name` }
-          >{meal[name]}</p>
+          >{recipe[name]}</p>
         </div>
       ))}
     </div>
+  </>
   );
 }
 
--- src/services/fetchAPI.ts
+++ src/services/fetchAPI.ts
@@ -10,10 +10,30 @@ export const getDrinksAPI = async () => {
   return data.drinks;
 };
 
-export const getRecipes = (location: any) => {
-  if (location.pathname === '/meals') {
-    return getMealsAPI();
+// export const getRecipes = (path: any) => {
+//   if (path === '/meals') {
+//     return getMealsAPI();
+//   } else {
+//     return getDrinksAPI();  
+//   }
+// }
+
+export const getMealsCategoriesAPI = async () => {
+  const response = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+  const data = await response.json();
+  return data.meals;
+}
+
+export const getDrinksCategoriesAPI = async () => {
+  const response = await fetch('https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list');
+  const data = await response.json();
+  return data.drinks;
+}
+
+export const getCategoties = (path: any) => {
+  if (path === '/meals') {
+    return getMealsCategoriesAPI();
   } else {
-    return getDrinksAPI();  
+    return getDrinksCategoriesAPI();  
   }
 }

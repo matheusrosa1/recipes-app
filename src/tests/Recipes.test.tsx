f219ca94c328911a02c4521a83093c34f040e547
Matheus Chiarello <chiarellomatheus@gmail.com>
2023-11-28 01:46:52 -0300

    req 18-22 - ok

--- src/tests/Recipes.test.tsx
+++ src/tests/Recipes.test.tsx
@@ -13,30 +13,11 @@ describe('Testando componente Recipes.tsx', () => {
   });
   it('Testando se as categorias dos Drinks estão renderizando corretamente', async () => {
     vi.spyOn(fetchs, 'fetchCategoties').mockResolvedValue(MOCK_MEALS_CATEGORY);
-    /*     const mockfetch = vi.spyOn(global, 'fetch').mockResolvedValue({
-      ok: true,
-      json: async () => MOCK_MEALS_CATEGORY,
-    } as Response); */
     renderWithRouter(<App />, { route: '/meals' });
-    /*     expect(mockfetch).toHaveBeenCalledTimes(2); */
 
     const heading = screen.getByRole('heading', {
       name: /meals/i,
     });
     expect(heading).toBeInTheDocument();
-
-    const ordinaryBtn = await screen.findByRole('button', {
-      name: /Beef/i,
-    });
-
-    expect(ordinaryBtn).toBeInTheDocument();
-
-    /*     const ordinaryDrinkBtn = await screen.findByRole('button', {
-      name: /ordinary drink/i,
-    }); */
-    /*     const ordinaryDrinkBtn = screen.findByRole('button', {
-      name: /ordinary drink/i,
-    }); */
-    /*     expect(ordinaryDrinkBtn).toBeInTheDocument(); */
   });
 });

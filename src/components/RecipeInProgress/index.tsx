f0c3983baa0cbae0d28393a1d9fcd3139e8f0af4
marcelaadriany <marcela.adriany@gmail.com>
2023-12-01 15:34:08 -0300

    feat - req 37 - renders checkboxes with the ingredients

--- src/components/RecipeInProgress/index.tsx
+++ src/components/RecipeInProgress/index.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate, useParams } from 'react-router-dom';
 import { Button } from '../Forms/Button';
 import { RecipeType } from '../types';
+import { getRecipesById } from '../../services/fetchAPI';
 
 function RecipeInProgress() {
   const { id } = useParams();
@@ -9,46 +10,58 @@ function RecipeInProgress() {
   const navigate = useNavigate();
   const [recipe, setRecipe] = useState<RecipeType[]>([]);
 
-  const recipeId = location.pathname === `/meals/${id}` ? 'idMeal' : 'idDrink';
+  const recipeId = location.pathname === `/meals/${id}/in-progress`
+    ? 'idMeal' : 'idDrink';
 
-  const renderIngredients = (recipeDetail: RecipeType) => {
-    const ingredients = Object.keys(recipeDetail)
-      .filter((key) => key.includes('strIngredient') && recipeDetail[key]);
-    const measures = Object.keys(recipeDetail)
-      .filter((key) => key.includes('strMeasure') && recipeDetail[key]);
+  const pathId = location.pathname === `/meals/${id}/in-progress`
+    ? `/meals/${id}`
+    : `/drinks/${id}`;
+
+  const getRecipes = async () => {
+    try {
+      const recipeById = await getRecipesById(pathId, id as string);
+      setRecipe(recipeById);
+      console.log(recipe);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const renderIngredientsAndMeasures = () => {
+    if (!recipe[0]) return null;
+    const ingredients = Object.keys(recipe[0]).filter(
+      (key) => key.includes('strIngredient') && recipe[0][key],
+    );
+    const measures = Object.keys(recipe[0]).filter(
+      (key) => key.includes('strMeasure') && recipe[0][key],
+    );
 
     return ingredients.map((ingredient, index) => (
       <label key={ index } data-testid={ `${index}-ingredient-step` }>
         <input type="checkbox" />
-        {`${recipeDetail[ingredient]} - ${recipeDetail[measures[index]]}`}
+        {`${recipe[0][ingredient]} - ${[recipe[0][measures[index]]]}`}
       </label>
     ));
   };
 
+  useEffect(() => {
+    getRecipes();
+  }, [location.pathname, id]);
+
   return (
     <div>
-      {recipe.map((recipeDetail: RecipeType, index) => (
-        <div key={ recipeDetail[recipeId] }>
-          <img
-            data-testid="recipe-photo"
-            src="alguma caminho"
-            alt="alguma coisa"
-          />
-          <h2
-            data-testid="recipe-title"
-          >
-            Título da receita
-          </h2>
-          <h3>Ingredientes:</h3>
-          {renderIngredients(recipeDetail)}
-          <p data-testid="recipe-category">Categoria</p>
-          <h2
-            data-testid="instructions"
-          >
-            Intruções
-          </h2>
-        </div>
-      ))}
+      <div>
+        <img
+          data-testid="recipe-photo"
+          src="alguma caminho"
+          alt="alguma coisa"
+        />
+        <h2 data-testid="recipe-title">Título da receita</h2>
+        <h3>Ingredientes:</h3>
+        {renderIngredientsAndMeasures()}
+        <p data-testid="recipe-category">Categoria</p>
+        <h2 data-testid="instructions">Intruções</h2>
+      </div>
       <Button
         dataTestId="share-btn"
         buttonLabel="Compartilhar"

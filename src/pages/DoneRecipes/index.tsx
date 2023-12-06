2fb5a87b4f6f54df6ae0cf0d9a4150b5da8bb02d
Matheus Rosa <matheusrosataxa@gmail.com>
2023-12-05 22:47:21 -0300

    requisitos 44 e 45 finalizados

--- src/pages/DoneRecipes/index.tsx
+++ src/pages/DoneRecipes/index.tsx
@@ -2,6 +2,8 @@ import { useContext } from 'react';
 import Header from '../../components/Header';
 import RecipesContext from '../../context/RecipesContext';
 import { Button } from '../../components/Forms/Button';
+/* import DoneRecipesRender from './DoneRecipesRender'; */
+import shareImage from '../../images/shareIcon.svg';
 
 function DoneRecipes() {
   const { doneRecipes, copyLinkDetail, copyMessage } = useContext(RecipesContext);
@@ -25,46 +27,100 @@ function DoneRecipes() {
         onClick={ () => console.log('oi') }
         buttonLabel="Drinks"
       />
-      {doneRecipes.map((doneRecipe, index) => (
-        <div key={ doneRecipe.id }>
-          <img
-            src={ doneRecipe.image }
-            alt={ doneRecipe.name }
-            height={ 200 }
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <p
-            data-testid={ `${index}-horizontal-top-text` }
-          >
-            {doneRecipe.category}
-          </p>
-          <h3
-            data-testid={ `${index}-horizontal-name` }
-          >
-            {doneRecipe.name}
-          </h3>
-          <p
-            data-testid={ `${index}-horizontal-done-date` }
-          >
-            {doneRecipe.doneDate}
-          </p>
-          <Button
-            dataTestId={ `${index}-horizontal-share-btn` }
-            buttonLabel="Compartilhar"
-            onClick={ () => () => console.log('oi') }
-          />
-          {copyMessage && (
-            <p>Link copied!</p>
-          )}
-          {doneRecipe.tags && doneRecipe.tags.map((tag) => (
-            <p
-              data-testid={ `${index}-${tag}-horizontal-tag` }
-              key={ tag }
-            >
-              {tag}
-            </p>
-          ))}
-        </div>
+      {doneRecipes && doneRecipes.map((doneRecipe, index) => (
+        doneRecipe.type === 'meal'
+          ? (
+            <div key={ doneRecipe.id }>
+              <img
+                src={ doneRecipe && doneRecipe.image }
+                alt=""
+                height={ 200 }
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {`${doneRecipe.nationality} - ${doneRecipe.category}`}
+              </p>
+              <h3
+                data-testid={ `${index}-horizontal-name` }
+              >
+                {doneRecipe.name}
+              </h3>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {doneRecipe.doneDate}
+              </p>
+              <input
+                type="image"
+                src={ shareImage }
+                className="btn-category"
+                alt="blackHeartIcon"
+                data-testid={ `${index}-horizontal-share-btn` }
+                onClick={ () => {} }
+                style={ { maxWidth: '100%', maxHeight: '100%' } }
+              />
+              {copyMessage && (
+                <p>Link copied!</p>
+              )}
+              {doneRecipe.tags && doneRecipe.tags.slice(0, 2)
+                .map((tag) => (
+                  <p
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                    key={ tag }
+                  >
+                    {tag}
+                  </p>
+                ))}
+            </div>
+          )
+          : (
+            <div key={ doneRecipe.id }>
+              <img
+                src={ doneRecipe && doneRecipe.image }
+                alt=""
+                height={ 200 }
+                data-testid={ `${index}-horizontal-image` }
+              />
+              <p
+                data-testid={ `${index}-horizontal-top-text` }
+              >
+                {doneRecipe.alcoholicOrNot}
+              </p>
+              <h3
+                data-testid={ `${index}-horizontal-name` }
+              >
+                {doneRecipe.name}
+              </h3>
+              <p
+                data-testid={ `${index}-horizontal-done-date` }
+              >
+                {doneRecipe.doneDate}
+              </p>
+              <input
+                type="image"
+                src={ shareImage }
+                className="btn-category"
+                alt="blackHeartIcon"
+                data-testid={ `${index}-horizontal-share-btn` }
+                onClick={ () => {} }
+                style={ { maxWidth: '100%', maxHeight: '100%' } }
+              />
+              {copyMessage && (
+                <p>Link copied!</p>
+              )}
+              {doneRecipe.tags && doneRecipe.tags.slice(0, 2)
+                .map((tag) => (
+                  <p
+                    data-testid={ `${index}-${tag}-horizontal-tag` }
+                    key={ tag }
+                  >
+                    {tag}
+                  </p>
+                ))}
+            </div>
+          )
       ))}
     </div>
   );

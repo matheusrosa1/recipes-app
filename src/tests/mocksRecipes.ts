d1a90a8d5b50660a86a6853be5a65e69b5606bcf
Mateus Felipe <mateus.mfr10@gmail.com>
2023-12-08 17:16:13 -0300

    feat: recipes test em andamento

---
+++ src/tests/mocksRecipes.ts
@@ -0,0 +1,136 @@
+const idMealString = '52977';
+const idDrinkString = '17222';
+const strMealString = 'Corba';
+const strDrinkString = 'A1';
+const strMealThumbString = 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg';
+const strDrinkThumbString = 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg';
+
+export const MOCK_RECIPES_TEST_MEALS = {
+  meals: [
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+    {
+      idMeal: idMealString,
+      strMeal: strMealString,
+      strMealThumb: strMealThumbString,
+    },
+  ],
+};
+
+export const MOCK_RECIPES_TEST_DRINKS = {
+  drinks: [
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+    {
+      idDrink: idDrinkString,
+      strDrink: strDrinkString,
+      strDrinkThumb: strDrinkThumbString,
+    },
+  ],
+};
